1cd4477bcea38cd831a657bc9ce068aa4762b156
Adjilino <ruiadelino.monte@gmail.com>
2023-12-02 12:54:49 +0000

    Delete day-02/target directory

--- day-02/target/debug/deps/day_02-b661bbec84e23698.d
+++
@@ -1,5 +0,0 @@
-/Users/adjilino/Code/advant-of-code/day-02/target/debug/deps/day_02-b661bbec84e23698: src/main.rs
-
-/Users/adjilino/Code/advant-of-code/day-02/target/debug/deps/day_02-b661bbec84e23698.d: src/main.rs
-
-src/main.rs:
